9e49c65fb97befcfc0bc1c617e60ece2e91a534d
Ryan Riahi <ryanr08@gmail.com>
2020-11-08 20:41:35 -0800

    Add comments

--- Lab2/clock_gen.v
+++ Lab2/clock_gen.v
@@ -91,12 +91,12 @@ module clock_div_two(
 	reg [3:0] a;
 	
 	always @ (posedge clk_in) begin
-		
+		//counter:
 		if (rst)
 			a <= 4'b0000;
 		else
 			a <= a + 1'b1;
-		
+		// set the values to the corresponding bits:
 		clk_div_2 <= a[0];
 		clk_div_4 <= a[1];
 		clk_div_8 <= a[2];
@@ -119,7 +119,7 @@ module clock_div_twenty_eight(
 		end
 		else begin
 			a <= a + 1'b1;
-			if (a[3:0] == 4'b1101) begin
+			if (a[3:0] == 4'b1101) begin	// when the counter is 13 we flip the divide by 28 clock and reset the counter
 				a <= 4'b0000;
 				clk_div_28 <= ~clk_div_28;
 			end
@@ -139,7 +139,7 @@ module clock_div_thirty_two(
 		end
 		else begin
 			a <= a + 1'b1;
-			if (a[3:0] == 4'b0000) begin
+			if (a[3:0] == 4'b0000) begin		// flip the divide by 32 clock whenever the counter overflows to 0000
 				clk_div_32 <= ~clk_div_32;
 			end
 		end
@@ -154,7 +154,7 @@ module clock_percent_thirty_three(
 	reg on;
 	reg on2;
 	reg [1:0] a2;
-
+	// 33% duty cycle triggered on a positive edge:
 	always @ (posedge clk_in) begin
 		if (rst) begin
 			a <= 2'b00;
@@ -164,15 +164,15 @@ module clock_percent_thirty_three(
 		end
 		else begin
 			a <= a + 1'b1;
-			if ((on == 1'b0 && a[1:0] == 2'b11) || (on == 1'b1 && a[1:0] == 2'b01)) begin
-				clk_percent_33 <= ~clk_percent_33_2;
-				on <= ~on;
+			if ((on == 1'b0 && a[1:0] == 2'b11) || (on == 1'b1 && a[1:0] == 2'b01)) begin		// if the signal is off and the counter is 3 or
+				clk_percent_33 <= ~clk_percent_33;						// when the signal is on and the counter is 2,
+				on <= ~on;									// we flip the 33% clocks
 				a <= 2'b00;
 			end
-			clk_or_33 <= (clk_percent_33 || clk_percent_33_2);
+			clk_or_33 <= (clk_percent_33 || clk_percent_33_2);					// take the or of the two clocks to generate a 41% duty cycle clock
 		end
 	end		
-	
+	// 33% duty cycle triggered on a negative edge:
 	always @ (negedge clk_in) begin
 		if (rst) begin
 			a2 <= 2'b00;
@@ -205,7 +205,7 @@ module clock_div_five (
 		end
 		else begin
 			a <= a + 1'b1;
-			if (a[2:0] == 3'b100) begin
+			if (a[2:0] == 3'b100) begin		// when the counter value is 4, we flip the divide by 5 clock and reset the counter
 				a <= 3'b000;
 				clk_div_5 <= ~clk_div_5;
 			end
@@ -217,7 +217,7 @@ module clock_div_two_hundred(
 	input clk_in, input rst, output reg clk_div_100, output reg clk_div_200
 	);
 	reg [6:0] a;
-	
+	// divide by 100 clock:
 	always @ (posedge clk_in) begin
 		if (rst) begin
 			a <= 7'b0000000;
@@ -225,7 +225,7 @@ module clock_div_two_hundred(
 		end
 		else begin
 			a <= a + 1'b1;
-			if (a[6:0] == 7'b1100100) begin
+			if (a[6:0] == 7'b1100100) begin			// when the counter is equal to 100, we reset it and flip the divide by 100 clock
 				a <= 7'b0000000;
 				clk_div_100 <= ~clk_div_100;
 			end
@@ -233,12 +233,12 @@ module clock_div_two_hundred(
 				clk_div_100 <= 1'b0;
 		end
 	end
-
+	// divide by 200 clock:
 	always @ (posedge clk_in) begin
 		if (rst)
 			clk_div_200 <= 1'b0;
 		else begin
-			if (clk_div_100)
+			if (clk_div_100)				// whenever the divide by 100 clock is active, we flip the divide by 200 clock
 				clk_div_200 <= ~clk_div_200;
 		end
 	end
@@ -258,13 +258,13 @@ module clock_strobe (
 		end
 		else begin
 			a <= a + 1'b1;
-			strobe <= a[1];
-			if (strobe) begin
+			strobe <= a[1];		
+			if (strobe) begin					// when strobe is active, we subtract by 5
 				toggle_counter <= toggle_counter - 3'b101;
 				strobe <= ~strobe;
 			end
 			else
-				toggle_counter <= toggle_counter + 2'b10;
+				toggle_counter <= toggle_counter + 2'b10;	// otherwise we add 2
 		end
 	end
 endmodule
